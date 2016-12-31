9628afedbdbbb2097d4ac8556be517c46d2818cc
relson816 <relson.batayola@gmail.com>
2016-12-31 12:07:05 +0800

    Edited the user model

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticable
 
 
-class User extends Model{
-	
+class User extends Model implements Authenticable{
+	use \Illuminate\Auth\Authenticable;
 }
