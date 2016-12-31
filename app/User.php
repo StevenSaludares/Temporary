f2c21a264789439844f84b04d3e2354f4e4ec74e
relson816 <relson.batayola@gmail.com>
2017-01-01 00:55:48 +0800

    Created a posts table and model

--- app/User.php
+++ app/User.php
@@ -8,4 +8,8 @@ use Illuminate\Contracts\Auth\Authenticatable;
 
 class User extends Model implements Authenticatable{
 	use \Illuminate\Auth\Authenticatable;
+	public function posts()
+	{
+		return $this->hasMany('App\Post');
+	}
 }
